d5fd83449d1ab17a68d91690bab53189bbf75c4f
Resister-boy <hassanpumped17@gmail.com>
2023-04-28 13:02:47 +0900

    get_next_line

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehulee <jaehulee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 17:34:15 by jaehulee          #+#    #+#             */
+/*   Updated: 2023/04/25 14:15:32 by jaehulee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen_ln(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_store_backup(char **backup)
+{
+	size_t	i;
+	size_t	ln;
+	size_t	after_ln;
+	char	*next_line;
+
+	if (!(*backup))
+		return (NULL);
+	i = 0;
+	ln = ft_strlen_ln(*backup);
+	if (!(*backup)[ln])
+	{
+		ft_free_backup(backup);
+		return (NULL);
+	}
+	after_ln = ft_stringlen(*backup) - (ln++);
+	next_line = ft_calloc((after_ln + 1), sizeof(char), backup);
+	if (!next_line)
+		return (NULL);
+	while ((*backup)[ln] != '\0')
+		next_line[i++] = (*backup)[ln++];
+	if (*backup)
+		ft_free_backup(backup);
+	return (next_line);
+}
+
+char	*ft_create_line(char **backup)
+{
+	char	*result;
+	size_t	ln;
+	size_t	i;
+
+	i = 0;
+	if (!(*backup) || !(*backup)[0])
+		return (NULL);
+	ln = ft_strlen_ln(*backup);
+	if ((*backup)[ln] == '\n')
+		ln += 1;
+	result = ft_calloc(ln + 1, sizeof(char), backup);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen_ln(*backup))
+	{
+		result[i] = (*backup)[i];
+		i++;
+	}
+	if ((*backup)[i] == '\n')
+		result[i] = '\n';
+	return (result);
+}
+
+char	*ft_read_line(int fd, char **backup)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		read_size;
+
+	if (!(*backup))
+		(*backup) = ft_calloc(1, sizeof(char), backup);
+	read_size = 1;
+	while (read_size > 0)
+	{
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size == -1)
+			return (NULL);
+		buffer[read_size] = '\0';
+		(*backup) = ft_strjoin_str(backup, buffer);
+		if (!(*backup))
+			return (NULL);
+		if (ft_check_ln(buffer) == 1)
+			return (*backup);
+	}
+	return (*backup);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*backup;
+	char		*result;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+	{
+		ft_free_backup(&backup);
+		return (0);
+	}
+	backup = ft_read_line(fd, &backup);
+	if (!backup)
+		return (0);
+	result = ft_create_line(&backup);
+	if (!result)
+	{
+		if (backup)
+			ft_free_backup(&backup);
+		return (0);
+	}
+	backup = ft_store_backup(&backup);
+	return (result);
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehulee <jaehulee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 17:36:31 by jaehulee          #+#    #+#             */
+/*   Updated: 2023/04/25 14:15:45 by jaehulee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin_str(char **backup, char *buf)
+{
+	char	*result;
+	int		total_len;
+	int		i;
+	int		j;
+
+	i = -1;
+	if (!(*backup) || !buf)
+	{
+		ft_free_backup(backup);
+		return (NULL);
+	}
+	total_len = ft_stringlen(*backup) + ft_stringlen(buf);
+	result = ft_calloc((total_len + 1), sizeof(char), backup);
+	if (!result)
+		return (NULL);
+	while ((*backup)[++i] != '\0')
+		result[i] = (*backup)[i];
+	j = -1;
+	while (buf[++j] != '\0')
+		result[i + j] = buf[j];
+	if (*backup)
+		ft_free_backup(backup);
+	return (result);
+}
+
+int	ft_check_ln(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_free_backup(char **backup)
+{
+	if (!(*backup))
+		return ;
+	free(*backup);
+	(*backup) = NULL;
+}
+
+void	*ft_calloc(size_t count, size_t size, char **backup)
+{
+	size_t	i;
+	size_t	result_len;
+	char	*result;
+
+	result = (char *)malloc(count * size);
+	if (!result)
+	{
+		if ((*backup) != NULL)
+			ft_free_backup(backup);
+		return (NULL);
+	}
+	i = 0;
+	result_len = ((count * size) / sizeof(char));
+	while (i < result_len)
+	{
+		result[i] = '\0';
+		i++;
+	}
+	return (result);
+}
+
+size_t	ft_stringlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
